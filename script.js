08911621af286fdc215b0efb7c8aba77eed3a315
Laraib <theyoungcoder24@gmail.com>
2023-02-20 13:16:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuTrigger = document.querySelector(".menu-trigger"),
+  closeTrigger = document.querySelector(".close"),
+  giveClass = document.querySelector(".site");
+menuTrigger.addEventListener('click', function() {
+  giveClass.classList.toggle('showmenu')
+})
+closeTrigger.addEventListener('click', function() {
+  giveClass.classList.remove('showmenu')
+})
+const io = new IntersectionObserver(entries =>{
+   entries.forEach(entry =>{
+      if(entry.intersectionRatio > 0){
+         entry.target.classList.add('this')
+      }
+   })
+})
+const boxEllist = document.querySelectorAll('.animate');
+boxEllist.forEach((el) => {
+   io.observe(el)
+})

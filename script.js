b037ef779cfbef67ebf08064edf922359d5d2eff
Laraib <theyoungcoder24@gmail.com>
2023-03-07 16:07:03 +0530

    new

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const menuTrigger = document.querySelector('.menu-trigger'),
   closeTrigger = document.querySelector('.close'),
   giveClass = document.querySelector('.site');
+  // menu open
 menuTrigger.addEventListener('click', function() {
   giveClass.classList.toggle('showmenu')
 })
+// menu close
 closeTrigger.addEventListener('click', function() {
   giveClass.classList.remove('showmenu')
 })
+
+// Animation
 const io = new IntersectionObserver(entries =>{
    entries.forEach(entry =>{
       if(entry.intersectionRatio > 0){
